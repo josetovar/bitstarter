1b44cd35fbee1c4effae0c5f2518842b5021ffa2
Jose Tovar <j.tovar.navarro@gmail.com>
2013-07-10 17:12:01 +0000

    web.js reading index.html

--- web.js
+++ web.js
@@ -1,11 +1,14 @@
 var fs = require('fs');
- var express = require('express');
- 
- var app = express.createServer(express.logger());
- 
- app.get('/', function(request, response) {
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
   var indexContents = fs.readFileSync('index.html');
   response.send(indexContents.toString());
- });
- 
- var port = process.env.PORT || 5000;
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
